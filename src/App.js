4c3540ee6e8f5ef9e1a26fee48e6c2e956e54a66
Pentti <poinonen0@gmail.com>
2021-07-20 18:07:24 +0300

    project skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [payload, setPayload] = useState({
+    name: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    setPayload({...payload, [e.target.name]: e.target.value});
+    console.log(payload.name);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      axios.post(
+        process.env.REACT_APP_API_URL,
+        { 'key1': `${payload.name}, ${payload.message}` }
+      );
+    } catch (err) {
+      console.log('err', err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          onChange={handleChange}
+          value={payload.name}
+        />
+
+        <label>Message:</label>
+        <input
+          type="text"
+          name="message"
+          onChange={handleChange}
+          value={payload.message}
+        />
+
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
