668a5a17c780474cb7c2eeafa36b7980c0eddff5
Pentti <poinonen0@gmail.com>
2021-07-22 19:33:31 +0300

    implement image sending s3

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 interface Item {
   name: string,
-  price: string
+  price: string,
+  image?: File
 }
 
 interface Payload extends Item {
@@ -13,11 +14,16 @@ interface Items {
   Items: Payload[]
 }
 
+interface SignUrl {
+  fileUploadURL: string
+}
+
 const App: React.FC = () => {
 
   const [payload, setPayload] = useState<Item>({
     name: '',
-    price: ''
+    price: '',
+    image: undefined
   });
 
   const [items, setItems] = useState<Payload[]>([]);
@@ -28,23 +34,57 @@ const App: React.FC = () => {
     setPayload({ ...payload, [e.target.name]: e.target.value });
   };
 
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    console.log(file);
+    setPayload({ ...payload, [e.target.name]: file });
+  };
+
+  // react dropzone
+
+  const uploadFile = async (): Promise<AxiosResponse | { error: string }> => {
+    // When the upload file button is clicked,
+    // first we need to get the presigned URL
+    // URL is the one you get from AWS API Gateway
+
+    try {
+      const { data } = await axios.get<SignUrl>(`${API_URL}/photos/initiate-upload?fileName=${payload?.image?.name}`);
+
+      // Getting the url from response
+      const url = data.fileUploadURL;
+
+      // Initiating the PUT request to upload file
+      return axios.put(url, payload.image, {
+        headers: {
+          headers: { 'Content-Type': 'multipart/form-data' }
+        }
+      });
+    } catch (err) {
+      console.log('uploadFileErr: ', err);
+      return { error: 'Error uploading file' };
+    }
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const item: Payload = {
+    uploadFile();
+    /* const item: Payload = {
       id: uuidv4(),
       name: payload?.name,
-      price: payload?.price
+      price: payload?.price,
+      image: payload?.image
     };
 
     try {
-      axios.put(
+      const data = await axios.put(
         `${API_URL}`,
         item
       );
+      console.log('data', data);
       setItems([...items, item]);
     } catch (err) {
       console.log('err', err);
-    }
+    } */
   };
 
   const getItems = async () => {
@@ -83,6 +123,13 @@ const App: React.FC = () => {
           name="price"
           onChange={handleChange}
           value={payload.price}
+        />
+
+        <label>Image:</label>
+        <input
+          type="file"
+          name="image"
+          onChange={handleFileChange}
         />
 
         <button type="submit">Send</button>
